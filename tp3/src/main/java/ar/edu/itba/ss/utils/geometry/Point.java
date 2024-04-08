26e5e9bfd1eeeb27be0481546e3e2dc6a73759b4
Elian Paredes <elianparedes.dev@gmail.com>
2024-04-08 11:51:01 -0300

    feat: add geometry utils

---
+++ tp3/src/main/java/ar/edu/itba/ss/utils/geometry/Point.java
@@ -0,0 +1,46 @@
+package ar.edu.itba.ss.utils.geometry;
+
+import java.util.Objects;
+
+public class Point {
+    private final double x;
+    private final double y;
+    private final double z;
+
+    public Point(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public Point(double x, double y){
+        this.x = x;
+        this.y = y;
+        this.z = 0.0;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double getZ() {
+        return z;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Point point = (Point) o;
+        return Double.compare(point.x, x) == 0 && Double.compare(point.y, y) == 0 && Double.compare(point.z, z) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y, z);
+    }
+}
