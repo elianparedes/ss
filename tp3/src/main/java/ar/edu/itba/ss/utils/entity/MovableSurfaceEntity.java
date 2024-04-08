2b13b3019400a1b11b3a561913185026fc06876f
Elian Paredes <elianparedes.dev@gmail.com>
2024-04-08 11:51:01 -0300

    feat: add geometry utils

---
+++ tp3/src/main/java/ar/edu/itba/ss/utils/entity/MovableSurfaceEntity.java
@@ -0,0 +1,35 @@
+package ar.edu.itba.ss.utils.entity;
+
+public class MovableSurfaceEntity<T extends Entity> extends SurfaceEntity<T> {
+
+    private double speed;
+
+    private double angle;
+    public MovableSurfaceEntity(T entity, double x, double y, double speed, double angle) {
+        super(entity, x, y);
+        this.angle = angle;
+        this.speed = speed;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public double getAngle() {
+        return angle;
+    }
+
+    public double getXSpeed(){
+        return Math.cos(angle)*speed;
+    }
+
+    public double getYSpeed(){
+        return Math.sin(angle)*speed;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("{ x: %.2f, y: %.2f, angle: %.2f, speed: %.2f, entity: %s }", this.getX(), this.getY(), this.angle,
+                this.speed,this.getEntity());
+    }
+}
---
+++ tp3/src/main/java/ar/edu/itba/ss/utils/geometry/Cell.java
@@ -0,0 +1,68 @@
+package ar.edu.itba.ss.utils.geometry;
+
+import ar.edu.itba.ss.utils.entity.Entity;
+import ar.edu.itba.ss.utils.entity.SurfaceEntity;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+public class Cell<T extends Entity> {
+
+    private final int i, j;
+
+    private final double size;
+    private final List<SurfaceEntity<T>> entities;
+
+    public Cell(int i, int j, double size) {
+        this.i = i;
+        this.j = j;
+        this.size = size;
+        this.entities = new ArrayList<>();
+    }
+
+    public void place(T entity, double x, double y) {
+        this.entities.add(new SurfaceEntity<>(entity, x, y));
+    }
+
+    public void place(SurfaceEntity<T> surfaceEntity) {
+        this.entities.add(surfaceEntity);
+    }
+
+    public int getI() {
+        return i;
+    }
+
+    public int getJ() {
+        return j;
+    }
+
+    public double getCenterX() {
+        return j * size + size / 2;
+    }
+
+    public double getCenterY() {
+        return i * size + size / 2;
+    }
+
+    public double getSize() {
+        return size;
+    }
+
+    public List<SurfaceEntity<T>> getEntities() {
+        return entities;
+    }
+
+
+    @Override
+    public String toString() {
+        return String.format("Cell [%d, %d] { %s }", i, j, Arrays.toString(entities.toArray()));
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(i, j, size, entities);
+    }
+}
+
