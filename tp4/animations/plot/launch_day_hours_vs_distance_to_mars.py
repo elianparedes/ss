e149b37215ea457a71eb67e81a22053bd9e4faf2
Elian Paredes <elianparedes.dev@gmail.com>
2024-05-09 16:45:53 -0300

    refactor: change launch day vs distance plots labels to spanish

--- tp4/animations/plot/launch_day_hours_vs_distance_to_mars.py
+++ tp4/animations/plot/launch_day_hours_vs_distance_to_mars.py
@@ -3,30 +3,31 @@ from matplotlib import pyplot as plt, ticker
 
 from utils.plots import set_global_font_size, set_plot_size
 
-csv_file = 'distances/min-distance-days.csv'
+csv_file = 'distances/min-distance-hour.csv'
 
 min_distance_days_data = pd.read_csv(csv_file)
 distance_values = min_distance_days_data['distance']
-day_values = min_distance_days_data['day']
+day_values = min_distance_days_data['hour']
+
+min_distance = min(distance_values)
+min_distance_index = distance_values.idxmin()
+min_day = day_values[min_distance_index]
 
 set_global_font_size(16)
 fig, ax = plt.subplots()
 set_plot_size(fig, 14, 8)
 
-plt.scatter(day_values, distance_values, s=4)
-plt.scatter(day_values[day_values == 176], distance_values[day_values == 176], color='red', s=50, label='Day 176')
+plt.scatter(day_values, distance_values, s=30)
+plt.scatter(day_values[day_values == min_day], distance_values[day_values == min_day], color='red', s=50, label='Day 176')
 
-ax.set_xlabel('Día de lanzamiento')
+ax.set_xlabel('Horas en el día de lanzamiento 176')
 ax.set_ylabel('Mínima distancia entre la nave y Marte (km)')
 ax.xaxis.set_major_formatter(ticker.ScalarFormatter(useMathText=True))
 plt.yscale('log')
 
-min_distance = min(distance_values)
-min_distance_index = distance_values.idxmin()
-min_day = day_values[min_distance_index]
 bbox_props = dict(boxstyle="round,pad=0.3", fc="white", ec="black", lw=1, alpha=0.9)
-ax.annotate(f'Mínima distancia: {min_distance:.2f} km\nen el día {min_day}',
-            xy=(min_day, min_distance), xytext=(min_day + 64, min_distance),
+ax.annotate(f'Mínima distancia: {min_distance:.2f} km\nen el día 176 a las {min_day}:00',
+            xy=(min_day, min_distance), xytext=(min_day + 1, min_distance),
             arrowprops=dict(facecolor='black', arrowstyle='-'),
             fontsize=12, ha='left', bbox=bbox_props)
 ax.axvline(x=min_day, color='gray', linestyle='--', label='')
--- tp4/animations/plot/launch_day_minutes_vs_distance_to_mars.py
+++ tp4/animations/plot/launch_day_minutes_vs_distance_to_mars.py
@@ -3,30 +3,31 @@ from matplotlib import pyplot as plt, ticker
 
 from utils.plots import set_global_font_size, set_plot_size
 
-csv_file = 'distances/min-distance-days.csv'
+csv_file = 'distances/min-distance-minute.csv'
 
 min_distance_days_data = pd.read_csv(csv_file)
 distance_values = min_distance_days_data['distance']
-day_values = min_distance_days_data['day']
+day_values = min_distance_days_data['minute']
 
 set_global_font_size(16)
 fig, ax = plt.subplots()
 set_plot_size(fig, 14, 8)
 
-plt.scatter(day_values, distance_values, s=4)
-plt.scatter(day_values[day_values == 176], distance_values[day_values == 176], color='red', s=50, label='Day 176')
+min_distance = min(distance_values)
+min_distance_index = distance_values.idxmin()
+min_day = day_values[min_distance_index]
 
-ax.set_xlabel('Día de lanzamiento')
+plt.scatter(day_values, distance_values, s=20)
+plt.scatter(day_values[day_values == min_day], distance_values[day_values == min_day], color='red', s=50, label='Day 176')
+
+ax.set_xlabel('Minutos en el día de lanzamiento 176 a las 8:00')
 ax.set_ylabel('Mínima distancia entre la nave y Marte (km)')
 ax.xaxis.set_major_formatter(ticker.ScalarFormatter(useMathText=True))
 plt.yscale('log')
 
-min_distance = min(distance_values)
-min_distance_index = distance_values.idxmin()
-min_day = day_values[min_distance_index]
 bbox_props = dict(boxstyle="round,pad=0.3", fc="white", ec="black", lw=1, alpha=0.9)
-ax.annotate(f'Mínima distancia: {min_distance:.2f} km\nen el día {min_day}',
-            xy=(min_day, min_distance), xytext=(min_day + 64, min_distance),
+ax.annotate(f'Mínima distancia: {min_distance:.2f} km\nen el día 176 a las 8:{min_day}',
+            xy=(min_day, min_distance), xytext=(min_day + 1, min_distance),
             arrowprops=dict(facecolor='black', arrowstyle='-'),
             fontsize=12, ha='left', bbox=bbox_props)
 ax.axvline(x=min_day, color='gray', linestyle='--', label='')
