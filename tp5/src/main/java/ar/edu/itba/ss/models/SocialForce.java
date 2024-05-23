dfba8c46659ed3c2923b0dd8322966f81ef35cf3
Elian Paredes <elianparedes.dev@gmail.com>
2024-05-23 18:40:33 -0300

    fix: vector operations in social force model calculation

--- tp5/src/main/java/ar/edu/itba/ss/models/SocialForce.java
+++ tp5/src/main/java/ar/edu/itba/ss/models/SocialForce.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class SocialForce {
 
     private static final double KN = 120000;
-    
+
     private static final double A = 2000;
 
     private static final double B = 0.08;
@@ -13,24 +13,24 @@ public class SocialForce {
 
     public static Vector calculateForce(Particle i, List<Particle> particles, double tau) {
 
-        Vector granularForce = calculateGranularForce(i,particles);
-        Vector socialForce = calculateSocialForce(i,particles);
+        Vector granularForce = calculateGranularForce(i, particles);
+        Vector socialForce = calculateSocialForce(i, particles);
         Vector drivingForce = i.getDesiredSpeedVector().sub(i.getVelocity()).multiply(i.getMass() / tau);
 
         return granularForce.sum(socialForce).sum(drivingForce);
     }
 
-    private static Vector calculateGranularForce(Particle i, List<Particle> particles){
-        Vector granularForce = new Vector(0,0);
+    private static Vector calculateGranularForce(Particle i, List<Particle> particles) {
+        Vector granularForce = new Vector(0, 0);
 
         for (Particle j : particles) {
-            if(!j.equals(i)){
+            if (!j.equals(i)) {
                 Vector normal = i.getPosition().sub(j.getPosition());
-                double epsilon = normal.norm() - (i.getRadius()+j.getRadius());
+                double epsilon = normal.norm() - (i.getRadius() + j.getRadius());
                 normal = normal.divide(normal.norm());
 
-                if(epsilon <= 0){
-                    granularForce.sum(normal.multiply(-1*epsilon*KN));
+                if (epsilon <= 0) {
+                    granularForce = granularForce.sum(normal.multiply(-1 * epsilon * KN));
                 }
 
             }
@@ -39,19 +39,19 @@ public class SocialForce {
         return granularForce;
     }
 
-    private static Vector calculateSocialForce(Particle i, List<Particle> particles){
+    private static Vector calculateSocialForce(Particle i, List<Particle> particles) {
 
-        Vector socialForce = new Vector(0,0);
+        Vector socialForce = new Vector(0, 0);
 
         for (Particle j : particles) {
-            if(!j.equals(i)){
+            if (!j.equals(i)) {
                 Vector normal = i.getPosition().sub(j.getPosition());
-                double epsilon = normal.norm() - (i.getRadius()+j.getRadius());
+                double epsilon = normal.norm() - (i.getRadius() + j.getRadius());
                 normal = normal.divide(normal.norm());
-                socialForce.sum(normal.multiply(A*Math.exp(-1*epsilon/B)));
-                }
-
+                socialForce = socialForce.sum(normal.multiply(A * Math.exp(-1 * epsilon / B)));
             }
+
+        }
 
         return socialForce;
     }
