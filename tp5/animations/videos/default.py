9609b308813e26cf5e370d0ed2dcf2873f6362bb
saulex16 <scastaneda@itba.edu.ar>
2024-06-03 02:30:44 -0300

    chore: change default video circles thickness

--- tp5/animations/videos/default.py
+++ tp5/animations/videos/default.py
@@ -11,7 +11,7 @@ video_name = "default.mp4"
 video_width = 1280
 video_height = 720
 
-show_numbers = True
+show_numbers = False
 
 
 def is_number(s: str) -> bool:
@@ -49,18 +49,22 @@ def draw_particles(video_builder: VideoBuilder, data: DataFrame):
         cv2.circle(video_builder.current_frame, (x, y), 8,
                    color, -1)
 
-        text = row['particle']
-        if is_number(text):
-            font = cv2.FONT_HERSHEY_SIMPLEX
-            font_scale = 0.5
-            font_thickness = 1
-            text_size, _ = cv2.getTextSize(text, font, font_scale, font_thickness)
-            text_x = x - text_size[0] // 2
-            text_y = y + text_size[1] // 2
+        cv2.circle(video_builder.current_frame, (x, y), 8,
+                   (0,0,0), 2)
 
-            # Draw the text
-            cv2.putText(video_builder.current_frame, text, (text_x, text_y), font, font_scale, (255, 255, 255), font_thickness,
-                        cv2.LINE_AA)
+        text = row['particle']
+        if show_numbers:
+            if is_number(text):
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                font_scale = 0.5
+                font_thickness = 1
+                text_size, _ = cv2.getTextSize(text, font, font_scale, font_thickness)
+                text_x = x - text_size[0] // 2
+                text_y = y + text_size[1] // 2
+
+                # Draw the text
+                cv2.putText(video_builder.current_frame, text, (text_x, text_y), font, font_scale, (255, 255, 255), font_thickness,
+                            cv2.LINE_AA)
 
     cv2.circle(video_builder.current_frame, ball, 5,
                (255, 255, 255), -1)
@@ -73,7 +77,7 @@ def render():
     field_image = cv2.imread('../assets/field.png')
     field_image = cv2.resize(field_image, (video_width, video_height))
 
-    simulation_file = '../../output/futball-vd3.00-tau0.50.csv'
+    simulation_file = '../../output/varying_speed-vd1.00-tau0.50.csv'
     data = pd.read_csv(simulation_file)
 
     timesteps = data['frame'].unique()
